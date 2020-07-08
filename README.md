5a6994a510eb1bcc3a7b80db0e3b4a91c392eb59
cyb <cliffbakalian@gmail.com>
2020-07-08 15:17:43 -0400

    read inputs from midi

---
+++ README.md
@@ -0,0 +1,10 @@
+# MIDI-LIGHTS
+
+I found a Evolution MK-225C MIDI keyboard laying around my house that no one was using so 
+I decided that I wanted to use it to trigger events.  
+
+What type of events you say?  
+Well who knows, but I think I want to control my lights, or at least my table lights with it.
+
+Should be fun.
+
---
+++ file.py
@@ -0,0 +1,24 @@
+import rtmidi
+
+midiin = rtmidi.RtMidiIn()
+
+def print_message(midi):
+    if midi.isNoteOn():
+        print('ON: ', midi.getMidiNoteName(midi.getNoteNumber()), midi.getVelocity())
+    elif midi.isNoteOff():
+        print('OFF:', midi.getMidiNoteName(midi.getNoteNumber()))
+#    elif midi.isController():
+#        print('CONTROLLER', midi.getControllerNumber(), midi.getControllerValue())
+
+ports = range(midiin.getPortCount())
+if ports:
+    for i in ports:
+        print(midiin.getPortName(i))
+    print("Opening port 0!") 
+    midiin.openPort(1)
+    while True:
+        m = midiin.getMessage(250) # some timeout in ms
+        if m:
+            print(m)
+else:
+    print('NO MIDI INPUT PORTS!')
